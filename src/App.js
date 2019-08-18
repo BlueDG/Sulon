9ed8e41adbec36586a50ddb27d440659c82d7dbb
BlueDG <Guillaume.dusseuxgicquel@gmail.com>
2019-08-18 20:31:13 +0200

    bootstrap + personnal font styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from 'react'
+import {Text, Title, Footer} from "./components/Text"
+
 
 function App() {
   return (
     <>
+    <Title>Sulon</Title>
+    <Text>Here is gathered some information regarding the Star Wars universe from Episode I to Episode VI.</Text>
+    <Footer className="fixed-bottom text-center py-3">Data from <a href="https://swapi.co/" className="card-link">SWAPI.co</a></Footer>
     
-
-
 
 
     </>
---
+++ src/components/Text.js
@@ -0,0 +1,22 @@
+import styled from '@xstyled/styled-components'
+
+
+export const Text = styled.div`
+    color: black;
+    text-align: center;
+`
+
+export const Title = styled.h1`
+    font-size: 40px;
+    text-align:center;
+    color: black;
+    text-transform: uppercase;
+    style={{ fontFamily: "title"}}
+`
+
+export const Footer = styled.div`
+    color: grey;
+    text-align: right;
+    background-color: lightgrey;
+
+`
