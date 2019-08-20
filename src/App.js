78c4d81ed643db883115b86f4136143502f814bf
BlueDG <Guillaume.dusseuxgicquel@gmail.com>
2019-08-20 20:03:12 +0200

    axios feat

--- src/App.js
+++ src/App.js
@@ -13,11 +13,7 @@ export default function App() {
   const [searchType, setSearchType] = useState("people");
 
   useEffect(()=> {
-    Axios.get('https://swapi.co/api/${searchType}', {
-      params: {
-        query: request,
-      },
-    })
+    Axios.get(`https://swapi.co/api/${searchType}/?search=${request}`)
     .then(res => setData(res.data.results))
     .catch(_=> setData([]))
     }, [request, searchType])
@@ -29,9 +25,21 @@ export default function App() {
     <Title>Sulon</Title>
     <Text>A repository of knowledge from the Star Wars universe from Episode I to Episode VI.</Text>
     <InputText setRequest={setRequest}/>
+    {Array.from(["people", "species", "starships", "planets", "vehicles", "films"]).map((e,i)=>{
+      return (
+        <div key={i}>
+          <label>{e}</label>
+          <input type="checkbox" value={e} checked={e === searchType} onChange={(e)=> setSearchType(e.target.value)}/>
+        </div>
+      )
+    })}
     </Container>
     <Container>
-    
+    {data.length > 0 && data.map((r,i)=>
+      <div key={i}>
+        {r.name}
+      </div>
+    )}
     </Container>
     <Footer className="fixed-bottom text-center py-1.5">Data from <a href="https://swapi.co/" className="card-link">SWAPI.co</a></Footer>
     </>
